26b67aa8a8976ad9c7ec53d58169ce04d0557766
Anton Gerasimov <chim@cpan.org>
2013-03-17 11:19:31 +0000

    Import of CHIM/Task-Cabinet-0.09 from CPAN.
    gitpan-cpan-distribution: Task-Cabinet
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         CHIM/Task-Cabinet-0.09.tar.gz
    gitpan-cpan-author:       CHIM
    gitpan-cpan-maturity:     released

--- lib/Task/Cabinet.pm
+++ lib/Task/Cabinet.pm
@@ -1,10 +1,13 @@
 package Task::Cabinet;
 
+# ABSTRACT: Install all the CPAN modules needed by Cabinet
+
 use v5.10.1;
 use warnings;
 use strict;
 
-our $VERSION = '0.07';
+our $VERSION = '0.09'; # VERSION
+our $AUTHORITY = 'cpan:CHIM'; # AUTHORITY
 
 1; # End of Task::Cabinet
 
@@ -18,26 +21,25 @@ Task::Cabinet - Install all the CPAN modules needed by Cabinet
 
 =head1 VERSION
 
-version 0.07
+version 0.09
 
 =head1 DESCRIPTION
 
 This L<Task> module installs all the dependencies for the stable version of Cabinet.
 
-=head1 AUTHOR
-
-Anton Gerasimov E<lt>chim@cpan.orgE<gt>
-
 =head1 SEE ALSO
 
 L<Task>
 
+=head1 AUTHOR
+
+Anton Gerasimov <chim@cpan.org>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2012 Anton Gerasimov.
+This software is copyright (c) 2012 by Anton Gerasimov.
 
-This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
-The full text of the license can be found in the LICENSE file included with this module.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
